aa765c996fb32e44c936448c461b11442b0e25f7
mvpraver <raverdarko@gmail.com>
2026-09-08 16:09:24 +0300

    Add AppDelegate implementation

---
+++ XiaomiRemote/Sources/AppDelegate.m
@@ -0,0 +1,12 @@
+#import "AppDelegate.h"
+#import "ViewController.h"
+
+@implementation AppDelegate
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    ViewController *vc = [ViewController new];
+    self.window.rootViewController = vc;
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+@end
